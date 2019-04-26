5dca7d6f67393fcb2574f1a73ff845684c0bafcc
Constantino Flouras <cpf19@pitt.edu>
2019-04-26 16:59:30 -0400

    Refactoring

--- MIDIAnalysis/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['process_5fargs',['process_args',['../main_8c.html#a8de011aeee37cd0347b5e01d7e3baed4',1,'main.c']]]
-];
--- MIDIAnalysis/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['midiblockstatus',['MIDIBlockStatus',['../structMIDIBlockStatus.html',1,'']]]
-];
